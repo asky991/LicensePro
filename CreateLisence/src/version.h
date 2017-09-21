16f2f8cb904f8d5bc58219a92b2b2bdca9541266
asky991 <991556955@qq.com>
2017-09-21 14:50:28 +0800

    编译

--- CreateLisence/src/version.h
+++ CreateLisence/src/version.h
@@ -1,7 +1,7 @@
 // version.rc.in
-#define VER_FILEVERSION             1,0,1,3
-#define VER_FILEVERSION_STR         "1.0.1.3\0"
+#define VER_FILEVERSION             1,0,2,1
+#define VER_FILEVERSION_STR         "1.0.2.1\0"
 
-#define VER_PRODUCTVERSION          1,0,1
-#define VER_PRODUCTVERSION_STR      "1.0.1\0"
+#define VER_PRODUCTVERSION          1,0,2
+#define VER_PRODUCTVERSION_STR      "1.0.2\0"
 //
